39a8228f5f7c6328ee8787e691d10b41c81aec9a
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2024-08-14 13:46:34 +0530

    Create Day3p1.kt
    Day 3 of class ,program 1-4

---
+++ Day3p1.kt
@@ -0,0 +1,36 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+//main can be written in two types:
+//1. with arguments
+//2. without arguments
+fun main(args:Array<String>) {
+    //println("Hello, world!!!")
+    var a=40
+    print(10)
+    println(a)
+    
+    //Error: Expecting property name or reciever type
+    //var try =20
+    //print(try)
+    
+    //keywords as identifiers using backticks (`)
+    var `class`="Hello"
+    println(`class`)
+    
+    //var v/s val
+    //var: variable is used to store variable
+    //val: (value) to store constants in which the values cannot be reassigned
+    
+    var myVar: Int=62
+    println(myVar)
+    
+    var var1=12//type inference is integer
+    var var2=67.738//type inference is float
+    var var3=true//type inference is boolean
+    var var4='Namaste' //type inference is String
+    
+    
+}
