47d15cce372b39dc467bd730bb53c30e6abea88c
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2024-08-26 13:09:26 +0530

    Logical operators

---
+++ Day6hw.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val a = true
+    val b = false
+
+    // Example of logical AND
+    val result1 = a && b // result1 will be false
+    println("a && b = $result1") // Output: a && b = false
+    
+     val result2 = a || b // result2 will be true
+    println("a || b = $result2") // Output: a || b = true
+    
+    
+}
