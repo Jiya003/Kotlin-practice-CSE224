921eebad8078ba179140e7b1d2e117e3b8d9ddce
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2024-08-27 13:47:45 +0530

    do while loop

---
+++ Day8p3.kt
@@ -0,0 +1,31 @@
+//Program 8
+//do-while loop
+//will execute atleast once, even if the condition is false
+//execute first, check after
+fun main(){
+  var i=5
+  do{
+      println("value: $i")
+      i=i-1
+  }while(i>5)
+}
+
+//Program 9
+//Task: print 5 4 3 2 1 using do while
+fun main(){
+  var i=5
+  do{
+      println("value: $i")
+      i=i-1
+  }while(i>0)// for 0 while(i>=0)
+}
+
+//Program 10
+//Task: print: 1 2 3 4 5 usind do while
+fun main(){
+  var i=1
+  do{
+      println("value: $i")
+      i=i+1
+  }while(i<6)
+}
