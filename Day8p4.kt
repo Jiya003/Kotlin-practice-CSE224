9084b02f170a34e6b7906e8c8bea90dc48b6583c
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2024-08-27 13:57:43 +0530

    Functions - Introduction

---
+++ Day8p4.kt
@@ -0,0 +1,12 @@
+//Program 11
+//functions
+//improves readability and maintainability
+//also known as subroutines or methods
+//They take input(parameters) and return output value(return type)
+fun demo(){
+    println("Hello, Kotlin, you are in demo function")
+}
+fun main(){
+  demo()
+  println("In main function")
+}
