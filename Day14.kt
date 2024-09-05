7fe2a10f979da7d0649690f6231bb97e22d69c00
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2024-09-05 13:32:23 +0530

    Task

---
+++ Day14.kt
@@ -0,0 +1,17 @@
+//Design a Kotlin program for a store to determine eligibilty for a special offer based on the total amount and membership status
+fun Discount(amt: Int, membership:Boolean){
+    if(amt>=1000 && membership==true){
+        println("Customer has 20% discount")
+    }else if(amt>=1000 && membership==false){
+        println("Customer has 10% discount")
+    }
+    else{
+        println("Customer doesn't has 20% discount")
+    }
+}
+fun main(){
+    val amount=1500
+    val hasMembership=false
+    
+    Discount(amount, hasMembership)
+}
